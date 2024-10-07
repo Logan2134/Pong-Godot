805086eeb5036d2cf260a514439fca6de8db17d9
Logan2134 <Logan.gill2578@gmail.com>
2024-10-07 15:40:47 -0400

    Updated ball physics

--- Ball.gd
+++ Ball.gd
@@ -4,9 +4,11 @@ var speed = 300
 var initial_direction = Vector2(1, 0).rotated(randf() * PI / 4)  # Random angle
 
 func _ready():
-	apply_impulse(Vector2.ZERO, initial_direction * speed)
+	# Set the initial linear velocity
+	linear_velocity = initial_direction * speed
 
 func _integrate_forces(state):
+	# Reset position if it goes off screen (optional)
 	if global_position.x < 0 or global_position.x > get_viewport().size.x:
 		reset_ball()
 
